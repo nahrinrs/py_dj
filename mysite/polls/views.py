23dbcb424654980a74b5884094f3da50a90f744d
Nahrin <nahrinrs@yahoo.com>
2013-07-28 22:29:30 -0700

    change password

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from polls.models import Choice, Poll
 
